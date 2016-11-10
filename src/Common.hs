da8d2853636a283bbd95671bd01a93bf683cb10c
Matthew Pickering <matthewtpickering@gmail.com>
2016-11-10 00:42:26 +0000

    Database infrastrucutre, nearly finished

---
+++ src/Common.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Common where
+
+import Web.Twitter.Conduit hiding (map)
+
+import Control.Applicative
+import Control.Lens
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.CaseInsensitive as CI
+import qualified Data.Map as M
+import Network.HTTP.Conduit
+import qualified Network.URI as URI
+import System.Environment
+
+import qualified Data.Text as T (pack, concat)
+import qualified Data.Text.IO as T (putStrLn)
+import Data.Text (Text)
+import Data.Monoid
+
+getOAuthTokens :: IO (OAuth, Credential)
+getOAuthTokens = do
+    consumerKey <- getEnv' "OAUTH_CONSUMER_KEY"
+    consumerSecret <- getEnv' "OAUTH_CONSUMER_SECRET"
+    accessToken <- getEnv' "OAUTH_ACCESS_TOKEN"
+    accessSecret <- getEnv' "OAUTH_ACCESS_SECRET"
+    let oauth = twitterOAuth
+            { oauthConsumerKey = consumerKey
+            , oauthConsumerSecret = consumerSecret
+            }
+        cred = Credential
+            [ ("oauth_token", accessToken)
+            , ("oauth_token_secret", accessSecret)
+            ]
+    return (oauth, cred)
+  where
+    getEnv' = (S8.pack <$>) . getEnv
+
+getProxyEnv :: IO (Maybe Proxy)
+getProxyEnv = do
+    env <- M.fromList . over (mapped . _1) CI.mk <$> getEnvironment
+    let u = M.lookup "https_proxy" env <|>
+            M.lookup "http_proxy" env <|>
+            M.lookup "proxy" env >>= URI.parseURI >>= URI.uriAuthority
+    return $ Proxy <$> (S8.pack . URI.uriRegName <$> u) <*> (parsePort . URI.uriPort <$> u)
+  where
+    parsePort :: String -> Int
+    parsePort []       = 8080
+    parsePort (':':xs) = read xs
+    parsePort xs       = error $ "port number parse failed " ++ xs
+
+getTWInfoFromEnv :: IO TWInfo
+getTWInfoFromEnv = do
+    pr <- getProxyEnv
+    (oa, cred) <- getOAuthTokens
+    return $ (setCredential oa cred def) { twProxy = pr }
+
+postMessage :: Text -> IO ()
+postMessage t = do
+    status <- T.concat . map T.pack <$> getArgs
+    T.putStrLn $ "Post message: " <> status
+    twInfo <- getTWInfoFromEnv
+    mgr <- newManager tlsManagerSettings
+    res <- call twInfo mgr $ update status
+    print res
--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,80 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 
 module Main (main) where
 
-import Control.Lens (to, only,(^?),ix, toListOf, Fold, filtered, lengthOf)
-import Data.HashMap.Strict
+import Control.Lens (to, only,(^?),ix, toListOf, Fold, filtered
+                    , lengthOf, has, view, (^.), _Just)
+import Data.HashMap.Strict (HashMap)
 import Data.ByteString.Lazy (ByteString)
-import Data.Text (Text, strip)
-import qualified Data.Text as T (drop)
+import Data.Text (Text, strip, unpack)
+import qualified Data.Text as T (drop, concat, intercalate, uncons, break, lines, take, length)
+import qualified Data.Text.IO as T (writeFile, readFile)
 import Data.Text.Read
 import Data.Text.Encoding.Error (lenientDecode)
 import Data.Text.Lazy.Encoding (decodeUtf8With)
 import Network.HTTP.Client (Response)
 import Network.Wreq (responseBody, get)
 import Text.Taggy (Node)
-import Text.Taggy.Lens (html, elements, children, contents,allNamed, allAttributed, named, content)
+import Text.Taggy.Lens (html, elements, children, contents,allNamed, allAttributed, named, content, attrs)
 import Text.Taggy.DOM
 import Data.Monoid
+import Control.Concurrent
+import Control.Monad (forM, void)
+import Data.List
 
 import Debug.Trace
+import Common
 
-scoreRow :: Fold Element Element
-scoreRow = filtered (\e -> lengthOf (allNamed (only "td")) e == 4)
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.ToRow
+import Database.PostgreSQL.Simple.ToField
+import Database.PostgreSQL.Simple.FromField
+import GHC.Generics ( Generic)
+
+
+databaseConfig = "host='localhost' port=5432 dbname='ultimate'"
+
+mkConnection :: IO Connection
+mkConnection = connectPostgreSQL databaseConfig
+
 
 data Score = Score {
-  team1name :: Text
-  , team2name :: Text
-  , team1score :: Int
-  , team2score :: Int } deriving Show
+    leagueName :: Text
+  , date :: Text
+  , team1name :: TeamId
+  , team2name :: TeamId
+  , score :: (Int, Int)
+   } deriving (Eq, Show)
+
+instance ToRow Score where
+  toRow (Score a b c d (e, f)) =
+    [toField a, toField b, toField c, toField d, toField e, toField f]
+
+instance FromRow Score where
+  fromRow = do
+    pres <- Score <$> field <*> field <*> field <*> field
+    score <- (,) <$> field <*> field
+    return (pres score)
+
+
+-- Must only insert new values
+insertIntoDB :: Connection -> [Score] -> IO ()
+insertIntoDB c ss =
+  () <$ executeMany c "INSERT INTO scores values (?,?,?,?,?,?)" ss
+
+-- Get all the scores, there will never be that many so just get them all.
+getDBScores :: Connection -> IO [Score]
+getDBScores c = query_ c "SELECT * FROM scores"
+
 
 printScore :: Score -> String
-printScore (Score t1 t2 s1 s2) =
-  show t1 <> " " <> show s1 <> " - " <> show s2 <> " " <> show t2
+printScore (Score l d t1 t2 s1) =
+  show l <> show d <> show t1 <> " " <> show s1 <> " " <> show t2
 
 eitherToMaybe :: Either s a -> Maybe a
 eitherToMaybe (Left _) = Nothing
@@ -45,22 +89,64 @@ splitScore t =
       (s2, "") <- decimal (T.drop 3 r) -- drop 3 for -
       return (s1, s2)
 
+scoreRow :: Fold Element Element
+scoreRow = filtered (\e -> lengthOf (allNamed (only "td")) e == 4)
 
-scoreRowToResult :: [Node] -> Maybe Score
-scoreRowToResult row = do
-  team1 <- row ^? ix 1 . allNamed (only "a") . children . traverse . content
+scoreRowToResult :: Text -> [Node] -> Maybe Score
+scoreRowToResult date row = do
+  league <- row ^? ix 0 . allNamed (only "img") . attrs . ix "title"
+  team1 <- row ^? ix 1 . allNamed (only "a") . attrs . ix "href" . to urlToTeamId
   score <- row ^? ix 2 . allNamed (only "span") . children . traverse . content
-  team2 <- row ^? ix 3 . allNamed (only "a") . children . traverse . content
-  (t1s, t2s) <- splitScore score
-  return $ Score team1 team2 t1s t2s
+  team2 <- row ^? ix 3 . allNamed (only "a") . attrs . ix "href" . to urlToTeamId
+  pscore <- splitScore score
+  return $ Score league date team1 team2 pscore
+
+-- We pass in a URL and pick out the team id
+urlToTeamId :: Text -> TeamId
+urlToTeamId = TeamId . read . take 4 . drop (T.length sampleUrl) . unpack
+ where
+   sampleUrl = "TeamProfile.aspx?id="
 
 
-recentPackages :: Response ByteString -> [Maybe Score]
+teamScores :: Response ByteString -> [Score]
   --[Text.Taggy.DOM.Element]
-recentPackages = toListOf
-               $ responseBody . to (decodeUtf8With lenientDecode)
-               . html . allAttributed getInfoTable . allNamed (only "tr")
-               . scoreRow . children . to scoreRowToResult
+teamScores =
+  let
+    tableRows :: Response ByteString -> [Element]
+    tableRows =
+      toListOf
+        $ responseBody . to (decodeUtf8With lenientDecode)
+        . html . allAttributed getInfoTable . allNamed (only "tr")
+
+    getDate :: Element -> Text
+    getDate e = e ^. allNamed (only "em") . contents
+
+    groupedWithDate :: [Element] -> Maybe Text -> [Element] -> [(Text, [Element])]
+    groupedWithDate acc Nothing [] = []
+    groupedWithDate acc (Just hdr) [] = [(hdr, reverse acc)]
+    groupedWithDate acc hdr (e:es)
+      -- See header row
+      | has (attrs . ix "class") e
+      , Just thdr <- hdr =
+          (thdr, reverse acc) : groupedWithDate [] (Just (getDate e)) es
+      -- Initialisation
+      | has (attrs . ix "class") e
+      , Nothing <- hdr , null acc =
+          groupedWithDate [] (Just (getDate e)) es
+      | Nothing <- hdr = trace "No header row" (groupedWithDate es hdr acc)
+      | otherwise = groupedWithDate (e:acc) hdr es
+    processScoreRows :: Text -> [Element] -> [Score]
+    processScoreRows t es =
+      toListOf (traverse . scoreRow . children . to (scoreRowToResult t) . _Just) es
+  in
+    concatMap (uncurry processScoreRows) . groupedWithDate [] Nothing  . tableRows
+
+
+
+
+
+
+--               . scoreRow . children . to scoreRowToResult
 
 getInfoTable :: Fold (HashMap Text Text) ()
 getInfoTable = ix "class" . only "info-table"
@@ -68,10 +154,134 @@ getInfoTable = ix "class" . only "info-table"
 url1 =
   "http://www.bucs.org.uk//bucscore/Results.aspx?id=4604&ResultType=league"
 
-url =
-  "http://www.bucs.org.uk/bucscore/Results.aspx?id=5634&ResultType=league"
 
-run = get url >>= return . recentPackages
+data League = L Text Text deriving Show
+
+newtype TeamId = TeamId { getTeamId :: Int } deriving (Generic, Show, Eq)
+
+instance ToField TwitterHandle where
+    toField = toField . getTwitter
+instance ToField TeamId where
+    toField = toField . getTeamId
+
+instance FromField TwitterHandle where
+    fromField a b = TwitterHandle <$> (fromField a b)
+instance FromField TeamId where
+    fromField a b = TeamId <$> (fromField a b)
+
+newtype TwitterHandle = TwitterHandle { getTwitter :: Text } deriving (Generic, Show)
+
+data Team = Team {
+    teamName :: Text --
+  , teamId   :: TeamId
+  , teamTwitter :: Maybe TwitterHandle
+  , teamNumber :: Int --Whether 1s 2s etc
+  } deriving (Generic, Show)
+
+instance ToRow Team
+instance FromRow Team
+
+insertTeams :: Connection -> [Team] -> IO ()
+insertTeams c = void . executeMany c "INSERT INTO teams VALUES (?,?,?,?)"
+
+getTeams :: Connection -> IO [Team]
+getTeams c = query_ c "SELECT * FROM teams"
+
+initialiseTeams :: IO ()
+initialiseTeams = do
+  c <- mkConnection
+  teams >>= insertTeams c
+  close c
+
+
+
+leagueUrls :: [League]
+leagueUrls = [
+   (L "Northern 1A" "5485")
+ , (L "Midlands 1A" "5486")
+ , (L "Western 1A" "5487")
+ , (L "South Eastern 1A" "5488")
+ , (L "Northern 2A" "5489")
+ , (L "Northern 2B" "5490")
+ , (L "Midlands 2A" "5491")
+ , (L "Midlands 2B" "5492")
+ , (L "Western 2A" "5493")
+ , (L "Western 2B" "5494")
+ , (L "South Eastern 2A" "5495")
+ , (L "South Eastern 2B" "5496")
+ , (L "Scottish 1A" "5634")
+ ]
+
+teams :: IO [Team]
+teams = do
+  ls <- T.lines <$> T.readFile "team-info.csv"
+  return $ map mkTeam ls
+
+mkTeam :: Text -> Team
+mkTeam s =
+  let p = T.break (==',')
+      (name, s') = p s
+      (twitter, s'') = p (T.drop 1 s')
+      (n, s''') = p (T.drop 1 s'')
+      teamid = strip (T.drop 1 s''')
+      checkTwitter s@(T.uncons -> Just ('@',_)) = Just (TwitterHandle s)
+      checkTwitter _ = Nothing
+  in Team name (TeamId (read $ unpack teamid))  (checkTwitter twitter) (read (unpack n))
+
+
+mkLeagueUrl :: Team -> String
+mkLeagueUrl t =
+  concat ["http://www.bucs.org.uk/bucscore/Results.aspx?id="
+         , show (getTeamId (teamId t))
+         , "&ResultType=team"]
+
+getAllTeamScores :: [Team] -> IO [Score]
+getAllTeamScores ts =
+  concat <$> (forM ts $ \t -> do
+    traceShowM ("Processing", t)
+    threadDelay 1000000
+    teamScores <$> get (mkLeagueUrl t))
+
+--run = get (mkLeagueUrl (head leagueUrls))  >>= return . leagueScores (L "" "")
 
 main :: IO ()
-main = get url >>= print <$> recentPackages
+main = return ()
+
+calculuateNewResults :: [Score] -> [Score] -> [Score]
+calculuateNewResults oldscores newscores = newscores \\ oldscores
+
+postResult :: Score -> IO ()
+postResult = postMessage . formatScore
+
+formatScore :: Score -> Text
+formatScore = undefined
+
+realmain = do
+  forkIO $ mainLoop
+  threadDelay (floor 3.6e9)
+
+mainLoop :: IO ()
+mainLoop = do
+        c <- mkConnection
+        ts <- getTeams c
+
+        records <- getDBScores c
+        results <- nub <$> getAllTeamScores ts
+        let new_results = calculuateNewResults records results
+        insertIntoDB c new_results
+--        mapM_ print new_results
+--        mapM_ postResult new_results
+        return ()
+
+
+populateDatabase :: IO ()
+
+
+
+--- TODO
+--Make a team table and FromRow, ToRow instances
+--Insert all teams into table
+--update scores to take teams as arguments
+--update scores table for teams foreign key
+--dedup score lookups as the will appear twice, once for each team.
+--
