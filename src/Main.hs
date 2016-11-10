ace73c8480ba614eac03a112cd1d9dffcdbfd950
Matthew Pickering <matthewtpickering@gmail.com>
2016-11-10 10:12:49 +0000

    Getting ready to deploy

--- src/Main.hs
+++ src/Main.hs
@@ -2,26 +2,27 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 
-module Main (main) where
+module Main (main, initialiseTeams) where
 
-import Control.Lens (to, only,(^?),ix, toListOf, Fold, filtered
-                    , lengthOf, has, view, (^.), _Just)
+import Control.Lens (to, only,(^?),ix, toListOf, filtered
+                    , lengthOf, has, (^.), _Just, Traversal')
 import Data.HashMap.Strict (HashMap)
 import Data.ByteString.Lazy (ByteString)
 import Data.Text (Text, strip, unpack)
-import qualified Data.Text as T (drop, concat, intercalate, uncons, break, lines, take, length)
-import qualified Data.Text.IO as T (writeFile, readFile)
+import qualified Data.Text as T (drop, intercalate, uncons, break
+                                , lines, length, pack, append)
+import qualified Data.Text.IO as T (readFile)
 import Data.Text.Read
 import Data.Text.Encoding.Error (lenientDecode)
 import Data.Text.Lazy.Encoding (decodeUtf8With)
 import Network.HTTP.Client (Response)
 import Network.Wreq (responseBody, get)
 import Text.Taggy (Node)
-import Text.Taggy.Lens (html, elements, children, contents,allNamed, allAttributed, named, content, attrs)
+import Text.Taggy.Lens (html, children, contents,allNamed, allAttributed, content, attrs)
 import Text.Taggy.DOM
-import Data.Monoid
 import Control.Concurrent
 import Control.Monad (forM, void)
 import Data.List
@@ -36,7 +37,12 @@ import Database.PostgreSQL.Simple.ToField
 import Database.PostgreSQL.Simple.FromField
 import GHC.Generics ( Generic)
 
+import Data.IntMap (IntMap, (!))
+import qualified Data.IntMap as IntMap
+import qualified Data.ByteString as BS (ByteString)
 
+
+databaseConfig :: BS.ByteString
 databaseConfig = "host='localhost' port=5432 dbname='ultimate'"
 
 mkConnection :: IO Connection
@@ -44,11 +50,11 @@ mkConnection = connectPostgreSQL databaseConfig
 
 
 data Score = Score {
-    leagueName :: Text
-  , date :: Text
-  , team1name :: TeamId
-  , team2name :: TeamId
-  , score :: (Int, Int)
+    _leagueName :: Text
+  , _date :: Text
+  , _team1name :: TeamId
+  , _team2name :: TeamId
+  , _score :: (Int, Int)
    } deriving (Eq, Show)
 
 instance ToRow Score where
@@ -71,11 +77,6 @@ insertIntoDB c ss =
 getDBScores :: Connection -> IO [Score]
 getDBScores c = query_ c "SELECT * FROM scores"
 
-
-printScore :: Score -> String
-printScore (Score l d t1 t2 s1) =
-  show l <> show d <> show t1 <> " " <> show s1 <> " " <> show t2
-
 eitherToMaybe :: Either s a -> Maybe a
 eitherToMaybe (Left _) = Nothing
 eitherToMaybe (Right v) = Just v
@@ -89,7 +90,7 @@ splitScore t =
       (s2, "") <- decimal (T.drop 3 r) -- drop 3 for -
       return (s1, s2)
 
-scoreRow :: Fold Element Element
+scoreRow :: Traversal' Element Element
 scoreRow = filtered (\e -> lengthOf (allNamed (only "td")) e == 4)
 
 scoreRowToResult :: Text -> [Node] -> Maybe Score
@@ -122,7 +123,7 @@ teamScores =
     getDate e = e ^. allNamed (only "em") . contents
 
     groupedWithDate :: [Element] -> Maybe Text -> [Element] -> [(Text, [Element])]
-    groupedWithDate acc Nothing [] = []
+    groupedWithDate _acc Nothing [] = []
     groupedWithDate acc (Just hdr) [] = [(hdr, reverse acc)]
     groupedWithDate acc hdr (e:es)
       -- See header row
@@ -142,20 +143,8 @@ teamScores =
     concatMap (uncurry processScoreRows) . groupedWithDate [] Nothing  . tableRows
 
 
-
-
-
-
---               . scoreRow . children . to scoreRowToResult
-
-getInfoTable :: Fold (HashMap Text Text) ()
+getInfoTable :: Traversal' (HashMap Text Text) ()
 getInfoTable = ix "class" . only "info-table"
-
-url1 =
-  "http://www.bucs.org.uk//bucscore/Results.aspx?id=4604&ResultType=league"
-
-
-data League = L Text Text deriving Show
 
 newtype TeamId = TeamId { getTeamId :: Int } deriving (Generic, Show, Eq)
 
@@ -194,24 +183,6 @@ initialiseTeams = do
   close c
 
 
-
-leagueUrls :: [League]
-leagueUrls = [
-   (L "Northern 1A" "5485")
- , (L "Midlands 1A" "5486")
- , (L "Western 1A" "5487")
- , (L "South Eastern 1A" "5488")
- , (L "Northern 2A" "5489")
- , (L "Northern 2B" "5490")
- , (L "Midlands 2A" "5491")
- , (L "Midlands 2B" "5492")
- , (L "Western 2A" "5493")
- , (L "Western 2B" "5494")
- , (L "South Eastern 2A" "5495")
- , (L "South Eastern 2B" "5496")
- , (L "Scottish 1A" "5634")
- ]
-
 teams :: IO [Team]
 teams = do
   ls <- T.lines <$> T.readFile "team-info.csv"
@@ -220,13 +191,14 @@ teams = do
 mkTeam :: Text -> Team
 mkTeam s =
   let p = T.break (==',')
-      (name, s') = p s
+      (teamname, s') = p s
       (twitter, s'') = p (T.drop 1 s')
       (n, s''') = p (T.drop 1 s'')
       teamid = strip (T.drop 1 s''')
-      checkTwitter s@(T.uncons -> Just ('@',_)) = Just (TwitterHandle s)
+      checkTwitter t@(T.uncons -> Just ('@',_)) = Just (TwitterHandle t)
       checkTwitter _ = Nothing
-  in Team name (TeamId (read $ unpack teamid))  (checkTwitter twitter) (read (unpack n))
+  in Team teamname (TeamId (read $ unpack teamid))
+                   (checkTwitter twitter) (read (unpack n))
 
 
 mkLeagueUrl :: Team -> String
@@ -242,46 +214,51 @@ getAllTeamScores ts =
     threadDelay 1000000
     teamScores <$> get (mkLeagueUrl t))
 
---run = get (mkLeagueUrl (head leagueUrls))  >>= return . leagueScores (L "" "")
-
-main :: IO ()
-main = return ()
-
 calculuateNewResults :: [Score] -> [Score] -> [Score]
 calculuateNewResults oldscores newscores = newscores \\ oldscores
 
-postResult :: Score -> IO ()
-postResult = postMessage . formatScore
+postResult :: IntMap Team -> Score -> IO ()
+postResult ts = traceShowM . formatScore ts
 
-formatScore :: Score -> Text
-formatScore = undefined
+formatScore :: IntMap Team -> Score -> Text
+formatScore ts (Score league _date team1 team2 (s1, s2)) =
+  T.intercalate " " [ "Ultimate Result:"
+                    , T.append league ":", getTwitterDisplay team1
+                    , T.pack (show s1)
+                    , "-"
+                    , T.pack (show s2)
+                    , getTwitterDisplay team2
+                    , "#BUCSWednesday"]
+  where
+    getTwitterDisplay :: TeamId -> Text
+    getTwitterDisplay tid =
+      let Team{teamTwitter, teamName, teamNumber} = ts ! getTeamId tid
+      in case teamTwitter of
+           Nothing -> teamName
+           Just twitter -> T.intercalate " " [getTwitter twitter
+                                             , teamNumberToIndicator teamNumber]
 
-realmain = do
-  forkIO $ mainLoop
-  threadDelay (floor 3.6e9)
+    teamNumberToIndicator :: Int -> Text
+    teamNumberToIndicator n =
+      case n of
+        1 -> "1st"
+        2 -> "2nd"
+        3 -> "3rd"
+        4 -> "4th"
+        5 -> "5th"
+        _ -> "6th"
 
-mainLoop :: IO ()
-mainLoop = do
+
+main :: IO ()
+main = do
         c <- mkConnection
         ts <- getTeams c
 
         records <- getDBScores c
         results <- nub <$> getAllTeamScores ts
         let new_results = calculuateNewResults records results
-        insertIntoDB c new_results
---        mapM_ print new_results
---        mapM_ postResult new_results
-        return ()
+--        insertIntoDB c new_results
+        mapM_ print new_results
+        let teamMap = IntMap.fromList (map (\t -> (getTeamId (teamId t), t)) ts)
+        mapM_ (postResult teamMap >> threadDelay 1000000) new_results
 
-
-populateDatabase :: IO ()
-
-
-
---- TODO
---Make a team table and FromRow, ToRow instances
---Insert all teams into table
---update scores to take teams as arguments
---update scores table for teams foreign key
---dedup score lookups as the will appear twice, once for each team.
---
